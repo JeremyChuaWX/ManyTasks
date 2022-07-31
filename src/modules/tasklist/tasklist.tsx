108eafee129d7e7aef7af4ada2dc7db67e11888d
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-07-31 13:35:35 +0800

    filters array

--- src/modules/tasklist/tasklist.tsx
+++ src/modules/tasklist/tasklist.tsx
@@ -5,14 +5,37 @@ interface TaskListProps {
   tasks: Task[];
 }
 
+function FilterComponent({ title }: { title: string }) {
+  return (
+    <button className="bg-slate-400 text-white w-max px-4 py-1 rounded-full">
+      {title}
+    </button>
+  );
+}
+
+function FilterArrayComponent() {
+  return (
+    <div className="flex justify-center gap-4">
+      <FilterComponent title="Done" />
+      <FilterComponent title="Today" />
+      <FilterComponent title="Other" />
+    </div>
+  );
+}
+
 function Header() {
-  return <h1 className="text-5xl font-extrabold text-center">ManyTasks</h1>;
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-5xl font-extrabold text-center">ManyTasks</h1>
+    </div>
+  );
 }
 
 function TaskList({ tasks }: TaskListProps) {
   return (
     <div className="flex flex-col gap-12">
       <Header />
+      <FilterArrayComponent />
       <div className="flex flex-col gap-4">
         {tasks.map((task, index) => (
           <TaskComponent key={index} task={task} />
